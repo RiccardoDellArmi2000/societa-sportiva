3e09a55b91e2b5ffabe3604bd31f02ea8b854ddb
teresavaillant <ter.vaillant@stud.uniroma3.it>
2022-09-07 16:06:53 +0200

    hoscrittociao

--- src/main/java/it/uniroma3/siw/CorsiSportiviApplication.java
+++ src/main/java/it/uniroma3/siw/CorsiSportiviApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CorsiSportiviApplication {
-
+/*ciao*/
 	public static void main(String[] args) {
 		SpringApplication.run(CorsiSportiviApplication.class, args);
 	}
